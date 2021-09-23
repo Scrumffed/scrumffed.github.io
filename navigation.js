8bc97b2882218cdac7635609734d1dd08eed906a
Wafl <mlwellerb@gmail.com>
2021-09-24 00:17:07 +0200

    updated js

--- navigation.js
+++ navigation.js
@@ -1,25 +1,68 @@
-const bPath = "../";
+const backPath = "../";
+const svsPath = "./services";
+const proPath = "./projects";
+const orgPath = "./organisation";
+const cntPath = "./contact";
 
-window.onload = e =>{
+window.onload = () =>{
     prepButtons();
     console.log("Nav Ready!");
-};
+}
 
 function prepButtons(){
-    document.getElementById("backBtn").addEventListener("click",function(){
-        goBack(1);
-    });
+    const back1Btn = document.getElementById("backBtn");
+    const back2Btn = document.getElementById("2backBtn");
+    
+    const servicesBtn = document.getElementById("svsBtn");
+    const projectsBtn = document.getElementById("proBtn");
+    const oranisationBtn = document.getElementById("orgBtn");
+    const contactsBtn =document.getElementById("cntBtn");
 
-    document.getElementById("2backBtn").addEventListener("click",function(){
-        goBack(2);
-    });
-};
+    if(back1Btn){
+        back1Btn.addEventListener("click",function(){
+            goBack(1);
+        });
+    }
+    
+    if(back2Btn){
+        back2Btn.addEventListener("click",function(){
+            goBack(2);
+        });
+    }
 
+    if(servicesBtn){
+        servicesBtn.addEventListener("click",function(){
+            navTo(svsPath);
+        });
+    }
+
+    if(projectsBtn){
+        document.getElementById("proBtn").addEventListener("click",function(){
+            navTo(proPath);
+        });
+    }
+
+    if(oranisationBtn){
+        document.getElementById("orgBtn").addEventListener("click",function(){
+            navTo(orgPath);
+        });
+    }
+
+    if(contactsBtn){
+        document.getElementById("cntBtn").addEventListener("click",function(){
+            navTo(cntPath);
+        });
+    }
+}
 
 function goBack(amount){
-    let fullP = bPath;
+    let fullPath = backPath;
     for(i = 0; i < amount; i++){
-        fullP += bPath;
+        fullPath += backPath;
     }
-    window.location.replace(fullP);
-};
+    navTo(fullPath);
+}
+
+function navTo(path){
+    window.location.replace(path);
+}
