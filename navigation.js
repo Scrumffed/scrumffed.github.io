eae4ef468507235d2d95413e01e7b02e88a0c5f9
Marc Bertelsen <71696960+Wafl97@users.noreply.github.com>
2021-09-24 17:55:03 +0200

    more js stuff

--- navigation.js
+++ navigation.js
@@ -1,68 +1,70 @@
+//Paths
 const backPath = "../";
 const svsPath = "./services";
 const proPath = "./projects";
 const orgPath = "./organisation";
 const cntPath = "./contact";
 
-window.onload = () =>{
+//Buttons
+const back1Btn = document.getElementById("backBtn");
+const back2Btn = document.getElementById("2backBtn");
+
+const servicesBtn = document.getElementById("svsBtn");
+const projectsBtn = document.getElementById("proBtn");
+const oranisationBtn = document.getElementById("orgBtn");
+const contactsBtn = document.getElementById("cntBtn");
+
+window.onload = () => {
     prepButtons();
     console.log("Nav Ready!");
 }
 
-function prepButtons(){
-    const back1Btn = document.getElementById("backBtn");
-    const back2Btn = document.getElementById("2backBtn");
-    
-    const servicesBtn = document.getElementById("svsBtn");
-    const projectsBtn = document.getElementById("proBtn");
-    const oranisationBtn = document.getElementById("orgBtn");
-    const contactsBtn =document.getElementById("cntBtn");
-
-    if(back1Btn){
-        back1Btn.addEventListener("click",function(){
+function prepButtons() {
+    if (back1Btn) {
+        back1Btn.addEventListener("click", function () {
             goBack(1);
         });
     }
-    
-    if(back2Btn){
-        back2Btn.addEventListener("click",function(){
+
+    if (back2Btn) {
+        back2Btn.addEventListener("click", function () {
             goBack(2);
         });
     }
 
-    if(servicesBtn){
-        servicesBtn.addEventListener("click",function(){
+    if (servicesBtn) {
+        servicesBtn.addEventListener("click", function () {
             navTo(svsPath);
         });
     }
 
-    if(projectsBtn){
-        document.getElementById("proBtn").addEventListener("click",function(){
+    if (projectsBtn) {
+        document.getElementById("proBtn").addEventListener("click", function () {
             navTo(proPath);
         });
     }
 
-    if(oranisationBtn){
-        document.getElementById("orgBtn").addEventListener("click",function(){
+    if (oranisationBtn) {
+        document.getElementById("orgBtn").addEventListener("click", function () {
             navTo(orgPath);
         });
     }
 
-    if(contactsBtn){
-        document.getElementById("cntBtn").addEventListener("click",function(){
+    if (contactsBtn) {
+        document.getElementById("cntBtn").addEventListener("click", function () {
             navTo(cntPath);
         });
     }
 }
 
-function goBack(amount){
+function goBack(amount) {
     let fullPath = backPath;
-    for(i = 0; i < amount; i++){
+    for (i = 0; i < amount; i++) {
         fullPath += backPath;
     }
     navTo(fullPath);
 }
 
-function navTo(path){
+function navTo(path) {
     window.location.replace(path);
 }
